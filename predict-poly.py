1cc1a9c4b43c346bdc5f4c0302e1effa8b52abde
Curtis L. Olson <curtolson@flightgear.org>
2020-05-17 09:33:29 -0500

    A small script tweak to make the prediction points slightly more accurate and the updated plot for today.

--- predict-poly.py
+++ predict-poly.py
@@ -110,7 +110,7 @@ for iList, regionName in enumerate(regionList):
     print(regionName)
     color = colorList[iList]
     data = dfAll[param][regionName].values
-    data = data[:-1]
+    #data = data[:-1]
     date = dfAll[param][regionName].index
     print("len:", len(data))
     print(data[-fit_days:])
